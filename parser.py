23a8ef7535fb35608c3bc91b7581885175aa6241
Peter Brantsch <peter.brantsch@kabelmail.de>
2014-05-01 13:20:23 +0200

    somewhat successful text extraction

--- parser.py
+++ parser.py
@@ -1,23 +1,31 @@
+from traceback import print_exc as traceback_print_exc
 from bs4 import BeautifulSoup
+from bs4 import element as bs4_element
 from .dish import dish
 from .menu import menu
 from datetime import date
 import re
 
-date_rgx = re.compile("\D*(\d*)\.(\d*)\.")
+date_rgx = re.compile("\s*\D*(\d*)\.(\d*)\.\?")
 
 def parse(data):
 	soup = BeautifulSoup(data.encode(encoding="latin_1",errors="ignore"))
 	tbody = soup.body.table.tbody
-	cur_date = None
+	# extract the relevant table from HTML
+	table = []
 	for tr in tbody.children:
+		row = []
 		try:
-			#tds = list(tr.children)
-			for td in tr.children:
-				print(td.string,end="")
-				dishes.append(dish(
-			print('----------------',end="")
-		except: pass
-		#menu.append(dish(*(tr.children)))
-	#menu = list(soup.body.table.tbody.children)
-	return menu
+			if isinstance(tr,bs4_element.Tag):
+				for td in tr.children:
+					if isinstance(td,bs4_element.Tag):
+						tdstr = ' '.join(td.strings)
+						if tdstr:
+							row.append(tdstr.strip())
+				if any(row):
+					table.append(row)
+		except:
+			traceback_print_exc()
+	# walk the table
+	for row in table:
+		print(row)
